597e93c462617c02d8691896eca5caec2b0e1aef
Tanvirul Islam <tanvirul_islam@outlook.com>
2017-10-01 01:59:37 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 // this will grab the mongoose object from the mongoose.js file and set it to a constiable in this server.js file. It uses ES6 object destructuring and so is the same as writting const mongoose = require('./db/mongoose').mongoose
 const {mongoose} = require('./db/mongoose');
@@ -109,6 +110,33 @@ app.delete('/todos/:id', (req,res) => {
 
 });
 
+app.patch('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('Invalid Id');
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (todo) => {
+    if (!todo) {
+      return res.status(400).send()
+    }
+
+    res.send({todo});
+  }).catch( (error) => {
+    res.status(400).send()
+  });
+
+});
 
 
 
